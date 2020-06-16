2030478881ade3140594dd2c64f59fa974106cb9
Devansh Agarwal <devansh.kv@gmail.com>
2020-06-16 12:57:02 -0400

    Modified tests
    Signed-off-by: Devansh Agarwal <devansh.kv@gmail.com>

--- tests/test_autoencoder.py
+++ tests/test_autoencoder.py
@@ -5,9 +5,10 @@ import docclean
 
 def test_autoencoder():
     ae = docclean.autoencoder.Autoencoder()
-    clean_data = tf.data.Dataset.from_tensors(tf.random.normal(shape=(128, 256, 256, 3))).batch(32)
-    dirty_data = tf.data.Dataset.from_tensors(tf.random.uniform(shape=(128, 256, 256, 3))).batch(32)
+    clean_data = tf.data.Dataset.from_tensors(tf.random.normal(shape=(32, 256, 256, 3), dtype=tf.float32))
+    dirty_data = tf.data.Dataset.from_tensors(tf.random.uniform(shape=(32, 256, 256, 3), dtype=tf.float32))
     old_weights = ae.autoencoder_model.get_weights()
     ae.train_model(tf.data.Dataset.zip(dirty_data, clean_data), epochs=3)
     new_weights = ae.autoencoder_model.get_weights()
-    assert old_weights.any() != new_weights.any()
+    for x, y in zip(old_weights, new_weights):
+        assert (x - y).any() != 0
--- tests/test_cycle_gan.py
+++ tests/test_cycle_gan.py
@@ -7,10 +7,10 @@ def test_cyclegan():
     gan = docclean.cycle_gan.CycleGan()
     old_weights = [x.get_weights() for x in
                    [gan.generator_g, gan.generator_f, gan.discriminator_x, gan.discriminator_y]]
-    clean_data = tf.data.Dataset.from_tensors(tf.random.normal(shape=(128, 256, 256, 3))).batch(32)
-    dirty_data = tf.data.Dataset.from_tensors(tf.random.uniform(shape=(128, 256, 256, 3))).batch(32)
+    clean_data = tf.data.Dataset.from_tensors(tf.random.normal(shape=(32, 256, 256, 3), dtype=tf.float32))
+    dirty_data = tf.data.Dataset.from_tensors(tf.random.uniform(shape=(32, 256, 256, 3), dtype=tf.float32))
     gan.train(dirty_data, clean_data, epochs=3)
     new_weights = [x.get_weights() for x in
                    [gan.generator_g, gan.generator_f, gan.discriminator_x, gan.discriminator_y]]
     for x, y in zip(old_weights, new_weights):
-        assert x.all() != y.all()
+        assert (x - y).any() != 0
